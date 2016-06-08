c39addcbfa3b610df4aa0f0845e2e542cc2c2ac8
Robert <baxterrp@gmail.com>
2016-06-08 15:16:59 -0400

    Added movie to story

--- src/AdventureGame.java
+++ src/AdventureGame.java
@@ -26,18 +26,44 @@ public class AdventureGame {
 		response = scan.nextLine();
 
 		if (Character.toLowerCase(response.charAt(0)) == 'y') {
-			dinner = true;
 			if (returnRandom()) {
+				dinner = true;
 				System.out
 						.println("You safely drive downtown and enjoy a meal and tasty beverage.");
 			} else {
 				System.out
-						.println("The roads are closed for a parade or something, it wasn't meant to be.");
+						.println("Sorry "
+								+ player
+								+ ", the roads are closed for a parade or something, it wasn't meant to be.");
 			}
 		} else {
 			System.out.println("Alright, you're not hungry...");
 		}
-		System.out.println("Time to head to the movie!");
+		System.out
+				.println("Time to head to the movie!\nWhat movie would you like to see? There is a 1) a sci-fi and 2) some kids movie.");
+		int movie = scan.nextInt();
+		if (returnRandom()) {
+			System.out.print("Enjoy the ");
+			switch (movie) {
+			case 1:
+				System.out.print("sci-fi movie.");
+
+				break;
+			case 2:
+				System.out.print("kids movie");
+				break;
+			}
+		} else {
+			if (dinner) {
+				System.out
+						.println("Oh man, looks like the theater burned down. No movie for you. At least you had dinner");
+			} else {
+				System.out
+						.println("Oh man, looks like the theater burned down. No movie OR dinner!");
+			}
+		}
+
+		scan.close();
 
 	}
 }
